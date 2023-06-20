4c0cb660e3130fa0d755e8df2dc70359ef84c317
Iqbal Farissi <121147943+IqbalFarissi@users.noreply.github.com>
2023-06-20 20:36:58 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Iqbal Farissi\\kuliah\\Semester 6\\Aplikasi Bergerak\\Tugas\\uts_aplikasi_bergerak" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=D:\\Iqbal Farissi\\kuliah\\Semester 6\\Aplikasi Bergerak\\Tugas\\uts_aplikasi_bergerak"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Iqbal Farissi\\kuliah\\Semester 6\\Aplikasi Bergerak\\Tugas\\uts_aplikasi_bergerak\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Iqbal Farissi\\kuliah\\Semester 6\\Aplikasi Bergerak\\Tugas\\uts_aplikasi_bergerak"
+  "FLUTTER_TARGET=D:\\Iqbal Farissi\\kuliah\\Semester 6\\Aplikasi Bergerak\\Tugas\\uts_aplikasi_bergerak\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Iqbal Farissi\\kuliah\\Semester 6\\Aplikasi Bergerak\\Tugas\\uts_aplikasi_bergerak\\.dart_tool\\package_config.json"
+)
